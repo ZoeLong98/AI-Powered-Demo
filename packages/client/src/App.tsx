7b867bca067e87dd407960a012d9d80b29120540
Zoe Long <zeyulong0908@gmail.com>
2025-08-24 20:41:19 -0700

    feat: set up shadcn

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const [message, setMessage] = useState("");
@@ -9,7 +10,12 @@ function App() {
       .then((data) => setMessage(data.message));
   }, []);
 
-  return <p className="font-bold text-3xl">{message}</p>;
+  return (
+    <>
+      <p className="font-bold text-3xl">{message}</p>
+      <Button variant="outline">Click me</Button>
+    </>
+  );
 }
 
 export default App;
