06b728dfff06fcec4c7396715cd684695a25e339
Zoe Long <zeyulong0908@gmail.com>
2025-09-07 21:40:49 -0700

    f

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -10,6 +10,11 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': '',
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
