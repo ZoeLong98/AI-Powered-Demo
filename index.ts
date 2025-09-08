7345b6caabe3ee2bba54dc0050ddb442d93eda10
Zoe Long <zeyulong0908@gmail.com>
2025-09-07 17:57:19 -0700

    fix

--- index.ts
+++ index.ts
@@ -3,13 +3,13 @@ import concurrently from 'concurrently';
 concurrently([
   {
     name: 'server',
-    command: 'bun run dev',
+    command: 'npm run dev',
     cwd: 'packages/server',
     prefixColor: 'cyan',
   },
   {
     name: 'client',
-    command: 'bun run dev',
+    command: 'npm run dev',
     cwd: 'packages/client',
     prefixColor: 'green',
   },
