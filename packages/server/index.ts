46477f35db49416d569bd744b0e9640d1d171080
Zoe Long <zeyulong0908@gmail.com>
2025-09-07 22:33:29 -0700

    open cors

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -7,18 +7,7 @@ const cors = require('cors');
 dotenv.config();
 
 const app = express();
-
-// 配置 CORS，只允许特定域名访问
-const corsOptions: CorsOptions = {
-  origin:
-    process.env.NODE_ENV === 'development'
-      ? ['https://client-olive-rho.vercel.app', 'http://localhost:5173'] // 开发环境允许本地开发
-      : ['https://client-olive-rho.vercel.app'], // 生产环境只允许部署的域名
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json()); // act like a middle, parse the json object
 app.use(router);
